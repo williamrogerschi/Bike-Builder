d3111583014aab923def53664ca8a978a4b33020
Billy Rogers <william.rogers.chi@gmail.com>
2024-01-03 12:10:14 -0600

    trying again with Netlify deployment

--- bike-labs/src/global.js
+++ bike-labs/src/global.js
@@ -1,1 +1,8 @@
-export const BASE_URL = 'http://localhost:3001/'
+const BASE_URL = 'http://localhost:3001/'
+
+if(process.env.NODE_ENV === 'production') {
+    BASE_URL = 'https://bike-labs-4b05568e3735.herokuapp.com'
+}
+
+export { BASE_URL }
+
